775d88d49295d24afcf5073741c215e1a19d2c96
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-26 14:29:07 -0500

    done with business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,5 +1,20 @@
 import { features } from "../constants"
 import Button from "./Button"
+
+const FeatureCard = ({feature}) => {
+  return (
+    <div className="flex-1 flex p-6 hover:bg-slate-800 hover:rounded-[20px]">
+      <div className="w-[64px] h-[64px] flex justify-center items-center ss:mr-5 rounded-full bg-dimBlue">
+        <img src={feature.icon} className="w-[50%] h-[50%]" />
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-start">
+        <h3 className="font-poppins text-white text-[18px] font-semibold leading-[24px] mb-1">{feature.title}</h3>
+        <p className="font-poppins text-dimWhite text-[16px] w-full">{feature.content}</p>
+      </div>
+    </div>
+  )
+}
+
 const Business = () => {
   return (
     <section id="features" className="flex md:flex-row flex-col px-6 w-full">
@@ -10,22 +25,17 @@ const Business = () => {
           You do the business, <br className="sm:block hidden" />
           we&#39;ll handle the money.{" "}
         </h2>
-        <p className="font-poppins text-dimWhite text-[18px] w-full text-justify leading-[30px]">With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <p className="font-poppins text-dimWhite text-[18px] w-full text-justify leading-[30px]">
+          With the right credit card, you can improve your financial life by building credit, earning rewards and saving
+          money. But with hundreds of credit cards on the market.
+        </p>
         <Button styles="mt-10" />
       </div>
       {/* RIGHT SIDE */}
       <div className="flex-1 flex-rol justify-center items-start xs:ml-5 ">
-      {features.map(feature => (
-        <div key={feature.id} className="flex-1 flex p-6 hover:bg-slate-800 hover:rounded-[24px]">
-          <div className="flex justify-center items-center ss:mr-5 ">
-            <img src={feature.icon} alt="" />
-          </div>
-          <div className="flex-1 flex flex-col justify-center items-start">
-            <h3 className="font-poppins text-white font-semibold ">{feature.title}</h3>
-            <p className="font-poppins text-dimWhite font-normal text-[18px] w-full">{feature.content}</p>
-          </div>
-        </div>
-      ))}
+        {features.map((feature) => (
+          <FeatureCard feature={feature} key={feature.id} />
+        ))}
       </div>
     </section>
   )
