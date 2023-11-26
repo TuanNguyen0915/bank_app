4ca6985c726d831902bbd419ea36d805afacad38
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-26 06:15:53 -0500

    Create NavBar component with mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import NavBar from "./components/NavBar"
-
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedBackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  NavBar,
+  Stats,
+  Testimonials,
+} from "./components"
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
@@ -10,7 +23,7 @@ const App = () => {
       </div>
 
       <div className="bg-primary sm:px-16 px-6 flex justify-center items-start">
-        <div className="xl:max-w-[1280px] w-full">Stats Business Billing CardDeal Testimonial Clients CTA Footer</div>
+        <div className="xl:max-w-[1280px] w-full"></div>
       </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,57 @@
+import { useState } from "react"
+import { close, logo, menu } from "../assets"
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoo-bank" className="w-[124px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mr-10">
+          <a href="">Home</a>
+        </li>
+        <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mr-10">
+          <a href="">Features</a>
+        </li>
+        <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mr-10">
+          <a href="">Product</a>
+        </li>
+        <li className="font-poppins font-normal cursor-pointer text-[16px] text-white">
+          <a href="">Clients</a>
+        </li>
+      </ul>
+      {/* mobile responsive */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          onClick={() => setToggle(!toggle)}
+          className="w-[28px] h-[28px] object-contain"
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mb-10">
+              <a href="">Home</a>
+            </li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mb-10">
+              <a href="">Features</a>
+            </li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px] text-white mb-10">
+              <a href="">Product</a>
+            </li>
+            <li className="font-poppins font-normal cursor-pointer text-[16px] text-white">
+              <a href="">Clients</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
