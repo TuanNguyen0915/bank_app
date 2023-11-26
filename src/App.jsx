399b4279cdb7e2452772481873451611743cdd4c
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-26 05:01:05 -0500

    Build the structuge for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="sm:px-16 px-6 flex justify-center items-center">
+        <div className="xl:max-w-[1280px] w-full">
+          NavBar
+        </div>
+      </div>
+
+      <div className="bg-primary sm:px-16 px-6 flex justify-center items-start">
+        <div className="xl:max-w-[1280px] w-full">
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonial
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
